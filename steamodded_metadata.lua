3d442b9f1f48e31d5f8aa87ff46fdaa70efa4f3e
lmesh91 <89877664+MathIsFun0@users.noreply.github.com>
2024-05-31 09:03:20 -0400

    more bugfixes

--- steamodded_metadata.lua
+++ steamodded_metadata.lua
@@ -4,7 +4,7 @@
 --- MOD_AUTHOR: [MathIsFun_]
 --- MOD_DESCRIPTION: A mod that increases Balatro's score limit.
 --- PREFIX: talisman
---- VERSION: 1.1.9
+--- VERSION: 1.1.10
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
