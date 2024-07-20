9b2713812cf6e5f38097bb0be33e0bd68e633006
jenwalter666 <60117721+jenwalter666@users.noreply.github.com>
2024-07-20 21:29:30 +0100

    Update talisman.lua

--- talisman.lua
+++ talisman.lua
@@ -11,6 +11,35 @@ if nativefs.read(lovely.mod_dir.."/Talisman/config.lua") then
     end
 end
 
+SMODS.Sound({
+    key = "xchip",
+    path = "MultiplicativeChips.wav"
+})
+SMODS.Sound({
+    key = "echip",
+    path = "ExponentialChips.wav"
+})
+SMODS.Sound({
+    key = "eechip",
+    path = "TetrationalChips.wav"
+})
+SMODS.Sound({
+    key = "eeechip",
+    path = "PentationalChips.wav"
+})
+SMODS.Sound({
+    key = "emult",
+    path = "ExponentialMult.wav"
+})
+SMODS.Sound({
+    key = "eemult",
+    path = "TetrationalMult.wav"
+})
+SMODS.Sound({
+    key = "eeemult",
+    path = "PentationalMult.wav"
+})
+
 if not SpectralPack then
   SpectralPack = {}
   local ct = create_tabs
