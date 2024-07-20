1f2bdd1671a870197d277ba49c0f68626e0d7772
Victor Oleksiienko <uknownvector@gmail.com>
2024-07-20 17:54:31 +0300

    also use custom e switch point for table values (poker hand chips x mult)

--- talisman.lua
+++ talisman.lua
@@ -107,8 +107,8 @@ if Talisman.config_file.break_infinity then
       if type(num) == 'table' then
           num = to_big(num)
           G.E_SWITCH_POINT = G.E_SWITCH_POINT or 100000000000
-          if num < to_big(G.E_SWITCH_POINT) then
-              return nf(num:to_number())
+          if num < to_big(e_switch_point or G.E_SWITCH_POINT) then
+              return nf(num:to_number(), e_switch_point)
           else
             return Notations.Balatro:format(num, 3)
           end
